42517ca2899521739bf245ecea8ebf9f8acc99e8
Ralph Bolo <ralph.bolo@gmail.com>
2013-08-02 14:25:05 -0400

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :pins, :dependent => :destroy
 
   
 end
